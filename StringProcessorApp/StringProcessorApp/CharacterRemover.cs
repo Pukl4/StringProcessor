badf95a308b6960299419c7dab78f06f5147c4ee
Puki4 <nikitka2601@mail.ru>
2024-07-26 01:37:40 +0300

    Adding method for removing specific characters from a string

---
+++ StringProcessorApp/StringProcessorApp/CharacterRemover.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProcessorApp
+{
+    public class CharacterRemover                
+    {
+        public static string GetString( string row, string user_input_char)
+        {
+            string[] splitRow = user_input_char.Split(',');
+
+            for (int i = 0; i < splitRow.Length; i++)
+            {
+                if (row.Contains(splitRow[i]))
+                {
+                    do
+                    {
+                        var ind = row.IndexOf(splitRow[i]);
+                        var removed_row = row.Remove(ind, 1);
+                        row = removed_row;
+                    }
+                    while (row.Contains(splitRow[i]));
+                }
+            }
+            Console.WriteLine(row);
+            return row;
+        }
+        
+    }
+}
---
+++ StringProcessorApp/StringProcessorApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace StringProcessorApp
+{
+    public class Program
+    {
+        //reverse a given string without respecting the order
+        //of the words in a sentence (without leveraging any out-of-the-box C# methods).
+
+        //same as previous, but with respect to the order of the words.
+
+        //remove a given set of forbidden characters from a given string.
+
+        //calculate how many times each character is used in a given string.
+        static void Main(string[] args)
+        {
+            var CharacterRemover = new CharacterRemover();
+            var StringWithoutChars = CharacterRemover.GetString("afterdark impression", "a,d,c,e,f");
+                
+
+        }
+    }
+}
